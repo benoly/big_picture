354348b2930d463e38dd45351067ea380f922388
benoly <benoly@gmail.com>
2016-10-26 16:51:26 -0600

    10/26/16 4:53pm

--- models/caseModel.js
+++ models/caseModel.js
@@ -6,53 +6,99 @@ var caseSchema = new mongoose.Schema({
   court: String,
   judge: String,
   causeNo: String,
+  opposingCounsel: String,
   client: [{type: mongoose.Schema.Types.ObjectId, ref: "Client"}],
   caseDesc: String,
-  joinAmendDate: Date,
-  joinAmendDesc: String,
-  witDiscDate: Date,
-  witDiscDesc: String,
-  expWitDiscDate: Date,
-  expWitDiscDesc: String,
-  discDate: Date,
-  discDesc: String,
-  plDiscDate: Date,
-  plDiscDesc: String,
-  defDiscDate: Date,
-  defDiscDesc: String,
-  motDate: Date,
-  motDesc: String,
-  discMotDate: Date,
-  discMotDesc: String,
-  dispMotDate: Date,
-  dispMotDesc: String,
-  nonDispMotDate: Date,
-  nonDispMotDesc: String,
-  motLimDate: Date,
-  motLimDesc: String,
-  mediationDate: Date,
-  mediationDesc: String,
-  mediatorSelecDate: Date,
-  mediatorSelecDesc: String,
-  pretConsDate: Date,
-  pretConsDesc: String,
-  pretExchangeDate: Date,
-  pretExchangeDesc: String,
-  finPretOrdDate: Date,
-  finPretOrdDesc: String,
-  trialBrfDate: Date,
-  trialBrfDesc: String,
-  jurInstDate: Date,
-  jurInstDesc: String,
-  witListDate: Date,
-  witListDesc: String,
-  exListDate: Date,
-  exListDesc: String,
-  depoDesigDate: Date,
-  depoDesigDesc: String,
-  trialBeginDate: Date,
-  trialEndDate: Date,
-  trialDesc:String
+  trial: [{
+    beginDate: Date,
+    endDate: Date,
+    description: String
+  }],
+  event: [{
+    date: Date,
+    title: String,
+    description: String
+  }]
+  // witDisc: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // expertDiscl: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // discovery: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // plDiscovery: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // defDiscovery: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // motions: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // discMotions: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // dispMotions: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // nonDispMotions: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // motLim: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // mediation: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // mediatorSelect: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // pretConsult: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // pretExchange: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // finPretOrder: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // trialBrf: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // jurInst: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // witList: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // exhList: [{
+  //   date: Date,
+  //   description: String
+  // }],
+  // depoDesig: [{
+  //   date: Date,
+  //   description: String
+  // }],
 });
 
 module.exports = mongoose.model('Case', caseSchema);
